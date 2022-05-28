f8cd9f80b30fffb2473d3d3a93c7bac6946115b7
Iryna Kovalova <kovra777@gmail.com>
2022-05-28 23:27:02 +0300

    новые кнопки

--- js/program_btn-read-more.js .js
+++ js/program_btn-read-more.js .js
@@ -1,7 +1,7 @@
-let btn = document.querySelector('.about-us__button');
+let btn = document.querySelector('.program__btn-js');
 btn.onclick = function () {
-  let more = document.querySelector(".more");
-  let btn = document.querySelector(".about-us__button");
+  let more = document.querySelector(".more__prog");
+  let btn = document.querySelector(".program__btn-js");
   if (more.style.display === "inline") {
     btn.innerHTML = "Read More";
     more.style.display = "none"
